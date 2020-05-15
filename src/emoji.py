ff359397a097e905d52f4e2135de00a38789f972
Oliver Cordes <ocordes@astro.uni-bonn.de>
2020-05-15 11:20:09 +0200

    - start with the module

---
+++ src/emoji.py
@@ -0,0 +1,35 @@
+#
+# References:
+#   https://www.webfx.com/tools/emoji-cheat-sheet/
+#   http://www.unicode.org/emoji/charts/full-emoji-list.html
+
+# people
+
+bowtie = ''
+smile = '\U0001F604'
+simple_smile = '\U0001F642'
+laughing = '\U0001F606'
+blush = '\U0001F60A'
+smiley = '\U0001F603'
+relaxed = '\U0001F60C'
+smirk = ''
+heart_eyes = ''
+kissing_heart = ''
+kissing_closed_eyes = ''
+flushed = ''
+relieved = ''
+satisfied = ''
+grin = ''
+wink = ''
+stuck_out_tongue_winking_eye = ''
+stuck_out_tongue_closed_eyes = ''
+grinning = ''
+kissing = ''
+
+
+# objects
+
+
+
+# eat and drink
+#beer = :beer:
